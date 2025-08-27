e2dcadf4fe33ec9a9fbe58ffdba6222365e3f7d0
Tom Zhang <tzhang@daoai.com>
2025-08-26 17:41:37 -0700

    initial push

---
+++ docs/source/conf.py
@@ -0,0 +1,72 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'DaoAI PCB AOI User Manual'
+copyright = '2021-2024 DaoAI Robotics Inc.'
+author = 'DaoAI'
+
+release = '2025.2'
+version = '2025.2.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx_tabs.tabs',
+    'sphinxcontrib.video',
+    'sphinx.ext.autosectionlabel',
+    'sphinxemoji.sphinxemoji',
+    "sphinx_multiversion",
+]
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+html_static_path = ['_static']
+
+# -- Options for HTML output
+language = 'EN'
+html_search_language = 'en'
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# -- Options to Support pdf build in chinese
+latex_engine = 'lualatex'
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
+
+
+html_theme = 'sphinx_rtd_theme'
+
+latex_engine = 'lualatex'
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
+epub_show_urls = 'footnote'
+
+templates_path = [
+    "_templates",
+]
+
+# sphinx-multiversion
+# All branches except 'master'
+smv_branch_whitelist = r'^(?!chinese).*$'
---
+++ docs/source/index.rst
@@ -0,0 +1,26 @@
+DaoAI PCB AOI 用户手册
+==============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Introduction
+
+   introduce/quick_start/index
+   introduce/quick_start/release-notes
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Application
+
+   introduce/quick_start/offline_setup
+   data/workspace/index
+   data/start_project/index
+   model/model
+   data/manage/start
+   data/annotate_models/models
+   train/train_model/index.rst
+   deploy/deploy_device/index.rst
+
+
+
+
